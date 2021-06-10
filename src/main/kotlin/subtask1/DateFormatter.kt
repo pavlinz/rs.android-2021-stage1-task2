ce0bb9e72015d395f9d6eead3635cf7be4fc93a9
pavlinz <paulusvasilevskiy@gmail.com>
2021-06-10 11:37:58 +0300

    fix: fix bugs

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,18 +1,21 @@
 package subtask1
 
+import java.time.DateTimeException
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 class DateFormatter {
 
     fun toTextDay(day: String, month: String, year: String): String {
         return try {
-            if (month == "2" && day == "29") throw Exception()
-            val pattern: String = if (month.length == 1) "dd-M-yyyy" else "dd-MM-yyyy"
-            val formatter = DateTimeFormatter.ofPattern(pattern)
-            val date = LocalDate.parse("$day-$month-$year", formatter)
-            date.format(DateTimeFormatter.ofPattern("dd MMMM, EEEE"))
-        } catch (e: Exception) {
+            LocalDate.of(year.toInt(), month.toInt(), day.toInt()).format(
+                DateTimeFormatter.ofPattern(
+                    "d MMMM, EEEE",
+                    Locale("ru")
+                )
+            )
+        } catch (ex: DateTimeException) {
             "Такого дня не существует"
         }
     }
